0a092cb73450f8b4e0b4cc11c4603dc1d493962a
marunfaiv <owoowo700@gmail.com>
2022-09-20 19:01:28 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+// Detect touch device
+const isTouchDevice = () =>{
+    try{
+        // create touch event
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch(e){
+        return false;
+    }
+};
+
+// console.log(isTouchDevice());
+function getMousePos(e){
+    mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+    mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+    flashlight.style.setProperty("--Xpos", mouseX + "px");
+    flashlight.style.setProperty("--Ypos", mouseY + "px");
+}
+
+document.addEventListener("mousemove", getMousePos);
+document.addEventListener("touchmove", getMousePos)
